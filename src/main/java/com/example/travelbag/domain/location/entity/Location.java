f188e1216f2e6695bc75387c0ba340d3c4da45f4
seoyeon83 <mool848777@naver.com>
2025-01-03 20:34:47 +0900

    feat: 여행지 및 항공사 엔티티 설계 #4

---
+++ src/main/java/com/example/travelbag/domain/location/entity/Location.java
@@ -0,0 +1,58 @@
+package com.example.travelbag.domain.location.entity;
+
+import com.example.travelbag.domain.location_airline.entity.LocationAirline;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private String image_url;
+
+    @OneToMany(mappedBy = "location")
+    private List<LocationAirline> location_airlines = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+
+    public List<LocationAirline> getLocation_airlines() {
+        return location_airlines;
+    }
+
+    public void setLocation_airlines(List<LocationAirline> location_airlines) {
+        this.location_airlines = location_airlines;
+    }
+}
