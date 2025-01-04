d19dbf1db0bec9f9f71aa86865a4a8e866199165
GyeongJoon <jjangjoon328@naver.com>
2025-01-04 16:20:04 +0900

    first commit

---
+++ src/main/java/com/example/travelbag/TravelbagApplication.java
@@ -0,0 +1,13 @@
+package com.example.travelbag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelbagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelbagApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/travelbag/TravelbagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travelbag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelbagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
