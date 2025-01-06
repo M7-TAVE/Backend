c6b028ab96572fa56a5b4474b2859b4f3e2d17cf
seoyeon83 <mool848777@naver.com>
2025-01-07 03:45:58 +0900

    feat: 국가명, 통화코드 필드 추가 #3

--- src/main/java/com/example/travelbag/domain/location/dto/LocationResponseDTO.java
+++ src/main/java/com/example/travelbag/domain/location/dto/LocationResponseDTO.java
@@ -13,12 +13,16 @@ import java.time.LocalDateTime;
 public class LocationResponseDTO {
     private Long id;
     private String name;
+    private String country;
+    private String currency_unit;
 
     // Location 객체를 받아서 DTO 객체로 변환
     public static LocationResponseDTO of(Location location) {
         return LocationResponseDTO.builder()
                 .id(location.getId())
                 .name(location.getName())
+                .country(location.getCountry())
+                .currency_unit(location.getCurrency_unit())
                 .build();
     }
 }
