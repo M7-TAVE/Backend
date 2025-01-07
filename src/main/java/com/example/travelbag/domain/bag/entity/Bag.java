dcf56fcf038423cebca6d881779fabf2871f56af
User <jjangjoon328@naver.com>
2025-01-07 11:00:39 +0900

    feat: Bag 기능 구현 #8

--- src/main/java/com/example/travelbag/domain/bag/entity/Bag.java
+++ src/main/java/com/example/travelbag/domain/bag/entity/Bag.java
@@ -2,12 +2,17 @@ package com.example.travelbag.domain.bag.entity;
 
 import com.example.travelbag.domain.bag.dto.BagRequestDto;
 import com.example.travelbag.domain.common.model.BaseTimeEntity;
+import com.example.travelbag.domain.item.entity.Item;
 import com.example.travelbag.domain.member.entity.Member;
+import com.example.travelbag.global.enums.Template;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -25,6 +30,12 @@ public class Bag extends BaseTimeEntity {
     @ManyToOne
     @JoinColumn(name = "member_id")
     private Member member;
+
+    @Enumerated(EnumType.STRING)
+    private Template template;
+
+    @OneToMany(mappedBy = "bag", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Item> items = new ArrayList<>();
 
     public void updateName(BagRequestDto bagRequestDto) {
         if (bagRequestDto.getName() != null) {
