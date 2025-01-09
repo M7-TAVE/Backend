b3870578d27afa49d3f965a91ceba506217b7732
seoyeon83 <mool848777@naver.com>
2025-01-09 19:18:45 +0900

    feat: 어노테이션 위치 변경 및 파라미터 입력 방식 변경

--- src/main/java/com/example/travelbag/domain/restaurant/controller/RestaurantController.java
+++ src/main/java/com/example/travelbag/domain/restaurant/controller/RestaurantController.java
@@ -5,19 +5,20 @@ import com.example.travelbag.domain.restaurant.dto.RestaurantResponseDTO;
 import com.example.travelbag.domain.restaurant.service.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/restaurant")
 public class RestaurantController implements RestaurantApi {
 
     @Autowired
     private RestaurantService restaurantService;
 
     @Override
-    public ResponseEntity<List<RestaurantResponseDTO>> getRestaurantsByLocation(@RequestParam(value = "location_id") Long location_id) {
+    @GetMapping("/{location_id}")
+    public ResponseEntity<List<RestaurantResponseDTO>> getRestaurantsByLocation(@PathVariable Long location_id) {
         List<RestaurantResponseDTO> airlines = restaurantService.getRestaurantsByLocation(location_id);
         return ResponseEntity.ok(airlines);
     }
--- src/main/java/com/example/travelbag/domain/souvenir/controller/SouvenirController.java
+++ src/main/java/com/example/travelbag/domain/souvenir/controller/SouvenirController.java
@@ -5,19 +5,20 @@ import com.example.travelbag.domain.souvenir.dto.SouvenirResponseDTO;
 import com.example.travelbag.domain.souvenir.service.SouvenirService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/souvenir")
 public class SouvenirController implements SouvenirApi {
 
     @Autowired
     private SouvenirService souvenirService;
 
     @Override
-    public ResponseEntity<List<SouvenirResponseDTO>> getSouvenirsByLocation(@RequestParam(value = "location_id") Long location_id) {
+    @GetMapping("/{location_id}")
+    public ResponseEntity<List<SouvenirResponseDTO>> getSouvenirsByLocation(@PathVariable Long location_id) {
         List<SouvenirResponseDTO> airlines = souvenirService.getSouvenirsByLocation(location_id);
         return ResponseEntity.ok(airlines);
     }
