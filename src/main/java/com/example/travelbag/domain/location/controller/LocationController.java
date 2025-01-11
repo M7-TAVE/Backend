0cf58b3c35dd301a75710424af610f86521a0117
User <jjangjoon328@naver.com>
2025-01-11 12:13:33 +0900

    Update configuration and controller

--- src/main/java/com/example/travelbag/domain/location/controller/LocationController.java
+++ src/main/java/com/example/travelbag/domain/location/controller/LocationController.java
@@ -1,6 +1,5 @@
 package com.example.travelbag.domain.location.controller;
 
-import com.example.travelbag.domain.location.controller.api.LocationApi;
 import com.example.travelbag.domain.location.dto.AirlineResponseDTO;
 import com.example.travelbag.domain.location.dto.CurrencyInfoDTO;
 import com.example.travelbag.domain.location.dto.LocationResponseDTO;
@@ -13,24 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-public class LocationController implements LocationApi {
+public class LocationController{
 
     @Autowired
     private LocationService locationService;
 
-    @Override
     public ResponseEntity<List<LocationResponseDTO>> getLocations() {
         List<LocationResponseDTO> locations = locationService.getLocations();
         return ResponseEntity.ok(locations);
     }
 
-    @Override
     public ResponseEntity<List<AirlineResponseDTO>> getAirlinesByLocation(@RequestParam(value="location_id") Long location_id) {
         List<AirlineResponseDTO> airlines = locationService.getAirlinesByLocation(location_id);
         return ResponseEntity.ok(airlines);
     }
 
-    @Override
     public ResponseEntity<CurrencyInfoDTO> getExchangeRate(@RequestParam(value="location_id") Long location_id){
         CurrencyInfoDTO currency_info = locationService.getExchangeRate(location_id);
         return ResponseEntity.ok(currency_info);
