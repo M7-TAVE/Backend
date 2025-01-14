92b159d8393844f1ea47834cd25b94aa2b2a8244
seoyeon83 <mool848777@naver.com>
2025-01-14 14:48:31 +0900

    feat: api 엔드포인트 형식 변경 #42

--- src/main/java/com/example/travelbag/domain/souvenir/controller/SouvenirController.java
+++ src/main/java/com/example/travelbag/domain/souvenir/controller/SouvenirController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/souvenir")
+@RequestMapping("/api/souvenir")
 public class SouvenirController implements SouvenirApi {
 
     @Autowired
