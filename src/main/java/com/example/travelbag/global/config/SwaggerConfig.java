f9fb7cc2c1dce25b5b795b7e64878b84582e019c
seoyeon83 <mool848777@naver.com>
2025-01-16 13:14:37 +0900

    fix: CORS 해결을 위해 swagger 기본 url 설정

--- src/main/java/com/example/travelbag/global/config/SwaggerConfig.java
+++ src/main/java/com/example/travelbag/global/config/SwaggerConfig.java
@@ -3,8 +3,11 @@ package com.example.travelbag.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -13,7 +16,8 @@ public class SwaggerConfig {
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .components(new Components())
-                .info(apiInfo());
+                .info(apiInfo())
+                .servers(List.of(new Server().url("https://www.jionly.tech"))); // 기본 API URL 설정
     }
 
     public Info apiInfo() {
