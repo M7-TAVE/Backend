133ab230e91b49bd69f0c1a6606c1927c5282144
seoyeon83 <mool848777@naver.com>
2025-01-18 11:25:22 +0900

    chore: 프론트 로컬 url로 수정 #53

--- src/main/java/com/example/travelbag/global/security/SecurityConfig.java
+++ src/main/java/com/example/travelbag/global/security/SecurityConfig.java
@@ -24,6 +24,7 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final String base_url = "http://localhost:5174"; // https://m7-frontend.vercel.app
     private final CustomOAuth2UserService customOAuth2UserService;
 
     @Bean
@@ -33,7 +34,7 @@ public class SecurityConfig {
             public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                                 Authentication authentication) throws IOException, ServletException {
                 // Vite 프론트엔드로 리다이렉트
-                response.sendRedirect("https://m7-frontend.vercel.app");
+                response.sendRedirect(base_url);
             }
         };
     }
@@ -64,7 +65,7 @@ public class SecurityConfig {
                         .successHandler(oauth2AuthenticationSuccessHandler())
                 )
                 .logout(logout -> logout
-                        .logoutSuccessUrl("https://m7-frontend.vercel.app/login")
+                        .logoutSuccessUrl(base_url + "/login")
                         .invalidateHttpSession(true)
                         .deleteCookies("JSESSIONID")
                 );
@@ -78,7 +79,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://m7-frontend.vercel.app")
+                        .allowedOrigins(base_url)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
