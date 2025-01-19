c200a51e3d103c1a50fd28320ee17270d3895c74
User <jjangjoon328@naver.com>
2025-01-19 14:56:34 +0900

    fix: 수정 오류 해결 #82

--- src/main/java/com/example/travelbag/domain/recommendeditem/repository/RecItemRepository.java
+++ src/main/java/com/example/travelbag/domain/recommendeditem/repository/RecItemRepository.java
@@ -2,6 +2,7 @@ package com.example.travelbag.domain.recommendeditem.repository;
 
 import com.example.travelbag.domain.recommendeditem.entity.RecItem;
 import com.example.travelbag.global.enums.ItemCategory;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,4 +12,7 @@ import java.util.List;
 public interface RecItemRepository extends JpaRepository<RecItem, Long> {
 
     List<RecItem> findAllByCategory(ItemCategory category);
+
+    @Query("SELECT DISTINCT r FROM RecItem r WHERE r.category = :category")
+    List<RecItem> findDistinctByCategory(ItemCategory category);
 }
