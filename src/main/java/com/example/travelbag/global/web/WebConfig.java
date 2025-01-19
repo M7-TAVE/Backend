0f952cb8d6496a0f7808605e9595ed8fa261cc31
seoyeon83 <mool848777@naver.com>
2025-01-19 18:33:38 +0900

    fix: 사용하지 않는 라이브러리 호출 삭제

--- src/main/java/com/example/travelbag/global/web/WebConfig.java
+++ src/main/java/com/example/travelbag/global/web/WebConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.nio.charset.StandardCharsets;
